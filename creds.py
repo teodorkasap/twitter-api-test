1bb8f3d6d814e41ce5e63aa7d213bbc0144fd2a0
teodorkasap <erol.erten@gmail.com>
2020-01-15 14:25:36 +0300

    stream capture and print to screen working

--- creds.py
+++ creds.py
@@ -2,18 +2,19 @@ import re
 
 
 def read_file_to_list(file):
-    lineList = [line.rstrip('\n') for line in open(creds_file)]
+    lineList = [line.rstrip('\n') for line in open(file)]
     return lineList
 
-def get_creds():
+def get_creds(file_path):
     creds_list = read_file_to_list(file_path)
     creds = {}
     for var in creds_list:
         key = re.search('(^[^:]*)', var)
-        value = re.search('\:([a-zA-Z0-9_]+$)', var)
+        value = re.search(':(.*)', var)
         if key:
             key_name = str(key.group(1))
         if value:
             key_value = str(value.group(1))
         creds.update({key_name:key_value})
+    print(creds)
     return creds
--- main.py
+++ main.py
@@ -1,15 +1,31 @@
 import creds
-
-
-
+import tweepy
 """
 check requirements.txt for environment packages
 testing twitter streaming api with tweepy
 """
 
-# * Specify file, where credentials are saved and initialize a dics with these 
+# * Specify file, where credentials are saved and initialize a dics with these
 creds_file = "credentials.txt"
 creds = creds.get_creds(creds_file)
+print(creds.get("api_key"), " " ,creds.get("api_secret"))
+print(creds.get("access_token"), " " ,creds.get("access_secret"))
 
+
+auth = tweepy.OAuthHandler(creds.get("api_key"), creds.get("api_secret"))
+auth.set_access_token(creds.get("access_token"), creds.get("access_secret"))
+api = tweepy.API(auth)
+
+
+class MyStreamListener(tweepy.StreamListener):
+    def on_status(self, status):
+        print(status.text)
+    def on_error(self, status_code):
+        if status_code == 420:
+            return False
+
+stream_listener = MyStreamListener()
+stream = tweepy.Stream(auth=api.auth, listener=stream_listener)
+stream.filter(track=["python"],languages=['en'])
 
 
