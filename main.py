e46a1015f5a7072c446796d6683e461ab7f51c2d
teodorkasap <erol.erten@gmail.com>
2020-01-15 11:27:03 +0300

    getting credentials from txt file

---
+++ main.py
@@ -0,0 +1,14 @@
+import creds
+
+
+
+"""
+check requirements.txt for environment packages
+"""
+
+# * Specify file, where credentials are saved and initialize a dics with these 
+creds_file = "credentials.txt"
+creds = creds.get_creds(creds_file)
+
+
+
